533f92b8ec28f3ac4ea9c83c5dd15584c58f104d
Jason Short <jason.short@wpengine.com>
2020-11-20 15:49:45 -0600

    https://github.com/bmatheny/memkeys/pull/26/files

--- src/net/pcap_live.cpp
+++ src/net/pcap_live.cpp
@@ -34,9 +34,9 @@ void PcapLive::open()
   if (!state.checkAndSet(state_NEW, state_STARTING)) {
     logger->warning(CONTEXT, "Device already open");
   }
-  const char *dev = getInterfaceC();
-  logger->info(CONTEXT, "Opening device %s for capture", dev);
-  handle = pcap_open_live(dev,                      /* device to capture on */
+  string dev = getInterface();
+  logger->info(CONTEXT, "Opening device %s for capture", dev.c_str());
+  handle = pcap_open_live(dev.c_str(),              /* device to capture on */
                           config->getSnapLength(),  /* how many bytes per packet */
                           config->isPromiscuous(),  /* promiscuous */
                           config->getReadTimeout(), /* read timeout, in ms */
